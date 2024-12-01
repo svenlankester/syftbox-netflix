ff679ec74591136f4ca397aff6014ed4f2ccc742
Gustavo Bertoli <gubertoli@gmail.com>
2024-12-01 18:54:50 +0100

    mlp fedavg

--- aggregator/main.py
+++ aggregator/main.py
@@ -1,8 +1,11 @@
 # Refence: https://syftbox-documentation.openmined.org/cpu-tracker-2
 
 import os
+import re
+import joblib
 import numpy as np
 from pathlib import Path
+from syftbox.lib import Client
 
 API_NAME = os.getenv("API_NAME")
 
@@ -11,36 +14,90 @@ def network_participants(datasite_path: Path) -> list[str]:
     Network Participants Discovery:
     Retrieves a list of user directories (participants) in a given datasite path. 
     This function scans the network for all available peers by looking at directories in the datasite path.
+    
+    By looking for "api_data / API_NAME" only those from the specific app will be considered.
+    
     """
 
     entries = os.listdir(datasite_path)
     users = []
 
     for entry in entries:
-        if Path(datasite_path / entry).is_dir():
+        if Path(datasite_path / entry / "api_data" / API_NAME).is_dir():
             users.append(entry)
 
     return users
 
+def extract_number(file_name):
+    match = re.search(r'_(\d+)\.joblib$', file_name)
+    return int(match.group(1)) if match else -1
 
-def get_users_history(
+
+def get_users_mlp_parameters(
         datasites_path: Path, peers: list[str]
-) -> tuple[float, list[str]]:
+) -> tuple[list, list]:
+    """
+    This method retrieve the parameters from the local trained MLP. Those files have the names:
+    - netflix_mlp_weights_<NUM_SAMPLES>.joblib
+    - netflix_mlp_bias_<NUM_SAMPLES>.joblib
+
+    Returns a tuple of (weights, biases) from all participants
+    """
     
-    active_peers = []
+    weights = []
+    biases = []
+
     for peer in peers:
-        view_history_file = (
-            datasites_path / peer / "api_data" / API_NAME / "netflix_aggregated.npy"    # other options are netflix_reduced.npy or MLP weights
-        )
+        dir = datasites_path / peer / "api_data" / API_NAME
 
-        if not view_history_file.exists():
-            continue
+        weight = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f)) and "mlp_weights" in f]
+        bias = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f)) and "mlp_bias" in f]
+        weight = max(weight, key=extract_number, default=None)  # get the greater 
+        bias = max(bias, key=extract_number, default=None)      # get the greater
 
-        try:
-            data = np.load(view_history_file)
-            active_peers.append(peer)
-            # TODO :THE AGGREGATOR PROCESSING
-        except:
-            continue
+        weights.append(dir / weight)
+        biases.append(dir / bias)
 
-    return active_peers
+    return weights, biases
+
+def weighted_average(parameters, samples):
+    total_samples = sum(samples)
+    weighted_params = [
+        np.multiply(param, n / total_samples) for param, n in zip(parameters, samples)
+    ]
+    return np.sum(weighted_params, axis=0)
+
+
+def mlp_fedavg(weights: list, biases: list) -> tuple[list, list]:
+    """
+    FedAvg computes the weighted average of parameters (weights and biases) from multiple users.
+    The weights for averaging are proportional to the number of samples each user has.
+    """
+    samples = [extract_number(str(n)) for n in weights]
+    
+    weight_matrices = [joblib.load(weight_path) for weight_path in weights]
+    bias_vectors = [joblib.load(bias_path) for bias_path in biases]
+
+    fedavg_weights = [weighted_average([w[layer] for w in weight_matrices], samples) for layer in range(len(weight_matrices[0]))]
+    fedavg_biases = [weighted_average([b[layer] for b in bias_vectors], samples) for layer in range(len(bias_vectors[0]))]
+
+    return fedavg_weights, fedavg_biases
+    
+
+if __name__ == "__main__":
+    client = Client.load()
+
+    datasite_path = Path(client.datasite_path.parent)   # automatically retrieve datasites path
+
+    peers = network_participants(datasite_path)         # check participant of netflix trend
+    
+    # MLP use case -> FedAvg
+    weights, biases = get_users_mlp_parameters(datasite_path, peers)    # MLP: retrieve the path to weights and bias
+    fedavg_weights, fedavg_biases = mlp_fedavg(weights, biases)
+    
+    output_mlp_fedavg = client.datasite_path / "public"
+    
+    joblib.dump(fedavg_weights, output_mlp_fedavg / "netflix_mlp_fedavg_weights.joblib")
+    joblib.dump(fedavg_biases, output_mlp_fedavg / "netflix_mlp_fedavg_biases.joblib")
+
+    
