e3edd9235a5f53d06b9973095625afffa2f17fe7
Gustavo Bertoli <gubertoli@gmail.com>
2025-06-06 20:02:05 +0200

    Dynamically loading Top 5 from Aggregator to Participant Local Webpage

--- syftbox_netflix/app.py
+++ syftbox_netflix/app.py
@@ -1,10 +1,23 @@
 import os
+import jinja2
+import json
 from pathlib import Path
 from fastsyftbox import FastSyftBox
 from fastapi import Request
 from fastapi.responses import HTMLResponse
+from syft_core import Client as SyftboxClient
+from syft_core import SyftClientConfig
 
 APP_NAME = os.getenv("APP_NAME", "syftbox-netflix")
+AGGREGATOR_DATASITE = os.getenv("AGGREGATOR_DATASITE")
+
+config = SyftClientConfig.load()
+client = SyftboxClient(config)
+path_top5_dp = Path(client.datasite_path.parent / AGGREGATOR_DATASITE / "app_data" / APP_NAME / "shared" / "top5_series.json")
+
+with open(path_top5_dp, "r", encoding="utf-8") as f:
+    top5_dp = json.load(f)
+
 
 app = FastSyftBox(
     app_name=APP_NAME,
@@ -22,5 +35,16 @@ async def ui_home(request: Request):
     template_path = current_dir / "participant_utils" / "home.html"
     with open(template_path, encoding="utf-8") as f:
         template_content = f.read()
-        
-    return HTMLResponse(template_content)
+    
+    top_series = sorted(top5_dp, key=lambda x: x["count"], reverse=True)[:4]    # limited to top 4 and not 5 (for better visibility)
+
+    series_for_template = [
+        {"name": item["name"], "img": item["img"]}
+        for item in top_series
+    ]
+
+    template = jinja2.Template(template_content)
+
+    rendered_content = template.render(series=series_for_template)
+
+    return HTMLResponse(rendered_content)
